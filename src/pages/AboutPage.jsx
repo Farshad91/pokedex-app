ec90b2223fe22e2ef82d6d18c459a6db8b64e9f0
Farshad91 <farshadmail91@gmail.com>
2026-04-27 13:14:35 +0200

    fix base for github pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <main>
       <h1>About</h1>
-      <p>This app is a Pokedex built with React and Vite.</p>
+      <p>This app is a Pokedex and it is built with React and Vite.</p>
     </main>
   )
 }
